68091957e451d2703d92bba046002e13b817c642
kawsarkabir <devkawsarkabir@gmail.com>
2025-02-04 23:35:14 +0600

    dislay data show

--- recup-mern/module-41/src/App.tsx
+++ recup-mern/module-41/src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
 
 	return (
 		<div>
-			<h1>Explore Our Popular Products </h1>
+			<h1 className="text-3xl font-semibold text-center text-sky-500 py-6">Explore Our Popular Products </h1>
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-5 items-center justify-items-center py-16">
 				{
 					watchs?.map(productItems => <DisplayWatch key={productItems.id} productItems={productItems} />)
