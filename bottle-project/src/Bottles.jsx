25f3bd6f27112de3e3dd89a5ff7584d8f20a732a
kawsarkabir <devkawsarkabir@gmail.com>
2025-06-23 09:38:41 +0600

    feat(ui): handleAddToCart func add for buying product

--- bottle-project/src/Bottles.jsx
+++ bottle-project/src/Bottles.jsx
@@ -1,16 +1,27 @@
-import { use } from 'react';
+import { use, useState } from 'react';
 import Bottle from './Bottle';
 
 export default function Bottles({ bottlesPromise }) {
   const data = use(bottlesPromise);
-  const handleAddToCart = (bottle)=>{
-    console.log('click product detials:', bottle)
-  }
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+    console.log('click product detials:', cart);
+  };
   return (
-    <div className="grid grid-cols-4 gap-5 mt-10">
-      {data.map((bottle) => (
-        <Bottle key={bottle.id} bottle={bottle} handleAddToCart={handleAddToCart} />
-      ))}
-    </div>
+    <>
+      <h1>added to cart: {cart.length}</h1>
+      <div className="grid grid-cols-4 gap-5 mt-10">
+        {data.map((bottle) => (
+          <Bottle
+            key={bottle.id}
+            bottle={bottle}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </div>
+    </>
   );
 }
