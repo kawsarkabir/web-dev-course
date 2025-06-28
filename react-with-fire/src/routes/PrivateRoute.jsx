50ba20d35e223ace524dd6a3df322e436dbec09f
kawsarkabir <devkawsarkabir@gmail.com>
2025-06-29 03:25:04 +0600

    loading spinner done with project done

--- react-with-fire/src/routes/PrivateRoute.jsx
+++ react-with-fire/src/routes/PrivateRoute.jsx
@@ -1,12 +1,11 @@
+import LoadingSpinner from '@/components/LoadingSpinner';
 import { useAuth } from '@/context/AuthContext';
 import { Navigate } from 'react-router';
 
 export default function PrivateRoute({ children }) {
   const { user, loading } = useAuth();
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  if (loading) return <LoadingSpinner />;
 
   if (!user) {
     return <Navigate to="/signin" replace />;
